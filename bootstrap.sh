f0dd31eb4be21cb35dd0f38ca7f4bcc3ad7ed019
tonikalombo <toni@kalombo.me>
2018-09-21 01:49:25 +0200

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+
+sudo apt-get install python-pip 
+pip install picamera  
+sudo pip install flask 
+git clone https://github.com/miguelgrinberg/flask-video-streaming.git 
+mv app.py.use.me app.py
+python app.py 

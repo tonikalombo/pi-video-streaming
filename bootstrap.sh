9723d3a430ff645839fd8268b9857dadfaecd600
tonikalombo <toni@kalombo.me>
2018-09-21 01:57:02 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,6 @@ sudo apt-get install python-pip
 pip install picamera  
 sudo pip install flask 
 git clone https://github.com/miguelgrinberg/flask-video-streaming.git 
-mv app.py.use.me app.py
+mv app.py.use.me flask-video-streaming/app.py
+cd flask-video-streaming
 python app.py 
